7ec0212dc0bd3cacdb2b5bbd0fa7ffa3011a7b76
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-08-15 02:05:52 +0600

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,1 +1,34 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const { name, email, address, phone, message } = req.body;
+
+        // Set up the email transporter
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.process.env,
+                pass: process.env.process.env,
+            },
+        });
+
+        const mailOptions = {
+            from: email,
+            to: 'sheikhraju112255@gmail.com', // Replace with the recipient's email address
+            subject: `Message from ${name}`,
+            text: `Name: ${name}\nEmail: ${email}\nAddress: ${address}\nPhone: ${phone}\n\nMessage:\n${message}`,
+        };
+
+        try {
+            await transporter.sendMail(mailOptions);
+            res.status(200).json({ message: 'Email sent successfully' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error sending email' });
+        }
+    } else {
+        res.status(405).json({ error: 'Method not allowed' });
+    }
+}
